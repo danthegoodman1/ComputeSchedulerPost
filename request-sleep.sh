e48b035cf465691e485ff19a9b7657c650b51392
Dan Goodman <xxdanthegoodmanxx@gmail.com>
2024-02-19 21:30:36 -0500

    shorter sleep to prevent timeout

--- request-sleep.sh
+++ request-sleep.sh
@@ -2,10 +2,9 @@ curl -d '{
   "Task": "increment",
   "Payload": {
     "Num": 1,
-    "SleepSec": 10
+    "SleepSec": 4
   },
   "Requirements": {
-    "Region": "us-east",
     "Slots": 8
   }
 }' -H 'Content-Type: application/json' http://localhost:8080/schedule
